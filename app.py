dab9b9fae733a512137668a81ddbbb53c9d716b2
masterOrigen <167365847+masterOrigen@users.noreply.github.com>
2024-09-06 11:15:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ def init_connection():
         st.write("Iniciando conexión a la base de datos...")
         conn = mysql.connector.connect(
             host="chatgpt-do-user-3243287-0.c.db.ondigitalocean.com",
-            port=25060,  # Añadido el puerto correcto
+            port=25060,
             user="doadmin",
             password="AVNS_Vfn884XHZhbBRmGnXLo",
-            database="OrigenMedios"
+            database="OrigenMedios"  # Corregido el nombre de la base de datos
         )
         st.success("Conexión establecida exitosamente!")
         return conn
@@ -51,7 +51,7 @@ def run_query(query):
         st.stop()
 
 # Título de la aplicación
-st.title('Visualización de Datos de MySQL')
+st.title('Visualización de Datos de OrigenMedios')
 
 # Iniciar conexión
 conn = init_connection()
@@ -69,12 +69,12 @@ else:  # MegaTime2023
     mes_column = "Mes"
 
 # Ejecutar la consulta con la tabla seleccionada
-query = f"SELECT Año, `{mes_column}`, valor FROM {table_name} ORDER BY Año, `{mes_column}`;"
+query = f"SELECT Año, `{mes_column}`, `Inversion MP` FROM {table_name} ORDER BY Año, `{mes_column}`;"
 rows = run_query(query)
 
 if rows:
     # Convertir los resultados a un DataFrame de pandas
-    df = pd.DataFrame(rows, columns=['Año', 'Mes', 'Valor'])
+    df = pd.DataFrame(rows, columns=['Año', 'Mes', 'Inversion MP'])
 
     # Crear una columna de fecha combinando Año y Mes
     df['Fecha'] = pd.to_datetime(df['Año'].astype(str) + '-' + df['Mes'], format='%Y-%B', errors='coerce')
@@ -83,7 +83,7 @@ if rows:
     df = df.sort_values('Fecha')
 
     # Crear el gráfico con Plotly
-    fig = px.line(df, x='Fecha', y='Valor', title=f'Valores a lo largo del tiempo - {table_name}')
+    fig = px.line(df, x='Fecha', y='Inversion MP', title=f'Inversión MP a lo largo del tiempo - {table_name}')
 
     # Mostrar el gráfico en Streamlit
     st.plotly_chart(fig)
@@ -103,7 +103,7 @@ if rows:
                      (df['Fecha'] <= pd.to_datetime(date_range[1]))]
 
     # Actualizar el gráfico con los datos filtrados
-    fig_filtered = px.line(filtered_df, x='Fecha', y='Valor', title=f'Valores en el rango seleccionado - {table_name}')
+    fig_filtered = px.line(filtered_df, x='Fecha', y='Inversion MP', title=f'Inversión MP en el rango seleccionado - {table_name}')
     st.plotly_chart(fig_filtered)
 else:
     st.warning("No se encontraron datos en la tabla seleccionada.")
